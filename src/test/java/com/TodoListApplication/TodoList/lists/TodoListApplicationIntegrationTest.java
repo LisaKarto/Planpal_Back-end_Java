9ca9e52e88427efc6c713931d22959ecc62e880f
LisaKarto <101811602+LisaKarto@users.noreply.github.com>
2023-01-12 16:11:19 +0100

    no run integrationtests

--- src/test/java/com/TodoListApplication/TodoList/lists/TodoListApplicationIntegrationTest.java
+++ src/test/java/com/TodoListApplication/TodoList/lists/TodoListApplicationIntegrationTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -20,22 +21,22 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 public class TodoListApplicationIntegrationTest
 {
-    @Autowired
-    ListService service;
-    @Autowired
-    ListRepository repository;
-
-    // post
-    @Test
-     void insertListIntoDatabase() throws Exception{
-        ListModel ListToBeSaved = new ListModel(1,"List from Test","IntegrationTest");
-        service.saveList(ListToBeSaved);
-        assertThat(service.getList(1)).isNotNull();
-    }
-    // get by id
-    @Test
-     void getListByID() throws Exception{
-        assertThat(service.getList(1)).isNotNull();
-    }
+//    @MockBean
+//    ListService service;
+//    @MockBean
+//    ListRepository repository;
+//
+//    // post
+//    @Test
+//     void insertListIntoDatabase() throws Exception{
+//        ListModel ListToBeSaved = new ListModel(1,"List from Test","IntegrationTest");
+//        service.saveList(ListToBeSaved);
+//        assertThat(service.getList(1)).isNotNull();
+//    }
+//    // get by id
+//    @Test
+//     void getListByID() throws Exception{
+//        assertThat(service.getList(1)).isNotNull();
+//    }
 
 }
